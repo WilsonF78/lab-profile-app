94ddad4c212c821aa8c8d281de14a93d7ff1e0f6
WilsonF78 <fortes.wilson78@gmail.com>
2022-09-19 21:03:41 +0200

    It 1 => /api/users	{ image }

---
+++ profile-app-client/src/component/Homepage.js
@@ -0,0 +1,14 @@
+function Homepage() {
+  return (
+    <div className="">
+      <h1>IronProfile</h1>
+      <p>
+        Today we will create an app with authoritation adding some cool style!
+      </p>
+      <button>Sign up</button>
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Homepage;
